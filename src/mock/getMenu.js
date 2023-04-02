60bf2d56fd45ff0f08848d7855595c24935a9bc3
xcccch1 <852848642.qq.com>
2023-04-02 20:41:29 +0800

    home

--- src/mock/getMenu.js
+++ src/mock/getMenu.js
@@ -1,11 +1,11 @@
 const messagesList = {
     data: [
-        { id: 1, path: '@assets/icon/header1.png', text: '消息' },
-        { id: 2, path: '@assets/icon/header2.png', text: '日历' },
-        { id: 3, path: '@assets/icon/header3.png', text: '云文档' },
-        { id: 4, path: '@assets/icon/header4.png', text: '知识库' },
-        { id: 5, path: '@assets/icon/header5.png', text: '邮箱' },
-        { id: 6, path: '@assets/icon/header6.png', text: '问卷' }
+        { id: 1, path: require('../assets/icon/header1.png'), text: '消息' },
+        { id: 2, path: require('../assets/icon/header2.png'), text: '日历' },
+        { id: 3, path: require('../assets/icon/header3.png'), text: '云文档' },
+        { id: 4, path: require('../assets/icon/header4.png'), text: '知识库' },
+        { id: 5, path: require('../assets/icon/header5.png'), text: '邮箱' },
+        { id: 6, path: require('../assets/icon/header6.png'), text: '问卷' }
     ]
 }
 
