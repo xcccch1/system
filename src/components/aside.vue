959a0c2d158664addbb3a3b510596bb1d6c999e5
C <852848642@qq.com>
2023-04-04 19:43:48 +0800

    corporation

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -1,39 +1,56 @@
 <template>
-  <el-aside width="13vw">
-    <el-menu @open="handleOpen" @close="handleClose" active-text-color="#3370ff" unique-opened :collapse="isCollapse" :collapse-transition="false">
-      <el-submenu :index="item.id+''" v-for="item in menu" :key="item.id">
+  <el-aside :width="isCollapse ? 'fit-content' : '10vw'">
+    <el-menu @open="handleOpen" @close="handleClose" active-text-color="#3370ff" unique-opened :collapse="isCollapse"
+      :collapse-transition="true">
+      <el-submenu :index="item.id + ''" v-for="item in menu" :key="item.id">
         <template slot="title">
           <i :class="iconobj[item.id]"></i>
-          <span>{{item.authName}}</span>
+          <span>{{ item.authName }}</span>
         </template>
         <el-menu-item-group>
-          <el-menu-item :index="secendMenu.id+''" v-for="secendMenu in item.children" :key="secendMenu.id">{{secendMenu.authName}}</el-menu-item>
+          <el-menu-item :index="secendMenu.id + ''" v-for="secendMenu in item.children" :key="secendMenu.id">{{
+            secendMenu.authName }}</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
-      <el-submenu index="8" @click="a(this.isCollapse)">
-         <template slot="title">
-          <i :class="isCollapse?'el-icon-s-unfold':'el-icon-s-fold'"></i>
+      <el-submenu index="8" @click.native="isCollapse = !isCollapse">
+        <template slot="title">
+          <i :class="isCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'"></i>
         </template>
       </el-submenu>
     </el-menu>
+    {{ screenWidth }}
   </el-aside>
 </template>
 
 <script>
-import {getMenuAPI} from "@/api/aside"
+import { getMenuAPI } from "@/api/aside"
 export default {
   name: "Aside",
   data() {
     return {
-      menu:[],
-      iconobj:{},
-      isCollapse: true
+      menu: [],
+      iconobj: {},
+      isCollapse: true,
+      iconArr: ["el-icon-user-solid", "el-icon-s-check", "el-icon-s-goods", "el-icon-s-order", "el-icon-s-marketing"],
+      screenWidth: "",
+      screenHeight: ""
     }
   },
-  async created(){
-    const {data:menu} = await getMenuAPI()
+  async created() {
+    const { data: menu } = await getMenuAPI()
     this.menu = menu.data
     this.iconobj = this.icon(menu.data)
+  },
+  mounted() {
+    this.screenWidth = document.body.clientWidth;
+    this.screenHeight = document.body.clientHeight;
+    window.onresize = () => {
+      return (() => {
+        this.screenWidth = document.body.clientWidth;
+        this.screenHeight = document.body.clientHeight;
+      })();
+    };
+
   },
   methods: {
     handleOpen(key, keyPath) {
@@ -42,11 +59,10 @@ export default {
     handleClose(key, keyPath) {
       console.log(key, keyPath);
     },
-    icon(menu){
+    icon(menu) {
       const iconobj = {}
-      const iconArr = ["el-icon-user-solid","el-icon-s-check","el-icon-s-goods","el-icon-s-order","el-icon-s-marketing"]
-      menu.forEach((item,index) => {
-        iconobj[item.id+''] = iconArr[index]
+      menu.forEach((item, index) => {
+        iconobj[item.id + ''] = this.iconArr[index]
       });
       return iconobj
     }
@@ -57,24 +73,28 @@ export default {
 <style lang="less" scoped>
 .el-aside {
   padding: 10px;
-  background-color:#f2f3f5;
+  background-color: #f2f3f5;
 }
 
-/deep/ .el-menu{
+/deep/ .el-menu {
   overflow: hidden;
-  background-color:#f2f3f5;
+  background-color: #f2f3f5;
   border: none;
 }
 
-/deep/ .el-menu-item-group__title{
+/deep/.el-submenu__title {
+  min-width: 170px;
+}
+
+/deep/ .el-menu-item-group__title {
   display: none;
 }
 
 /deep/ .el-menu-item:hover {
-  background-color: rgba(31,35,41,.08);
+  background-color: rgba(31, 35, 41, .08);
 }
 
-/deep/ .el-submenu__title:hover{
-  background-color:rgba(31,35,41,.08) ;
+/deep/ .el-submenu__title:hover {
+  background-color: rgba(31, 35, 41, .08);
 }
 </style>
