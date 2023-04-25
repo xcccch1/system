36cfd883a634c2ee5cca7b6a1821645b8e6ed554
C <852848642@qq.com>
2023-04-25 20:00:19 +0800

    corporation

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,9 +1,12 @@
 import Mock from "mockjs";
 import { getmessages } from "./messages";
-import { getHeaderMenu } from "@/mock/getHeaderMenu"
+import { getHeaderMenu } from "./getHeaderMenu"
+import getTabList from "./tablist";
 
 // 
 Mock.mock(/\/getmessages/, "get", getmessages)
 
 // header
 Mock.mock(/\/getHeaderMenuList/, "get", getHeaderMenu)
+
+Mock.mock(/\/getTabList/,"get",getTabList)
---
+++ src/mock/tablist.js
@@ -0,0 +1,29 @@
+import Mock from "mockjs";
+// const Mock = require("mockjs")
+
+const tablist = [
+    Mock.mock({
+        title:"Message",
+        "number|10000-20000": 100,
+        icon:"#icon-xinxi"
+    }),
+    Mock.mock({
+        title:"Purchases",
+        "number|10000-20000": 100,
+        icon:"#icon-jifen"
+    }),
+    Mock.mock({
+        title:"Message",
+        "number|10000-20000": 100,
+        icon:"#icon-xinxi"
+    }),
+    Mock.mock({
+        title:"More",
+        number:null,
+        icon:"#icon-gengduo"
+    })
+]
+
+export default function getTabList(){
+    return tablist
+}
